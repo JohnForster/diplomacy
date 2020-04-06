a0ff2c3d1c57e2e7cdbfd1c8017a9ae8f0f0a167
John Forster <john.forster@vodafone.com>
2020-04-06 12:43:37 +0100

    update build command

--- build/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _server = _interopRequireDefault(require("./server/server"));
-
-(0, _server["default"])();
